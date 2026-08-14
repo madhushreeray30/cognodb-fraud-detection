316c057d46c3e33984c5e70ebb825db736816607
madhushreeray30 <madhushree@turbot.com>
2026-08-14 22:41:03 +0530

    Update to deploy

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const { getSession } = require('./db');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // Helper to convert BigInt to Number
 const convertBigInt = (obj) => {
